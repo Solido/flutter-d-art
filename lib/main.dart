b2a8614f5c8e02c2f5726334fdbdf6bad8cf2432
Robert Felker <felker.robert@gmail.com>
2019-09-03 10:26:25 +0200

    Documentation

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:vector_math/vector_math.dart' as v;
 
+// Look Ma NO WIDGET in Flutter !!!
 class Colored extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final simplex = v.SimplexNoise();
+    final random = v.SimplexNoise();
     final frames = 200;
     canvas.drawPaint(Paint()..color = Colors.black87);
 
@@ -19,17 +20,26 @@ class Colored extends CustomPainter {
 
       final area = Offset(i, i) & Size(i * 10, i * 10);
 
+      // Blue trail is made of rectangle
       canvas.drawRect(
           area,
           Paint()
-            ..filterQuality = FilterQuality.high
-            ..blendMode = BlendMode.screen
+            ..filterQuality =
+                FilterQuality.high // Change this to lower render time
+            ..blendMode =
+                BlendMode.screen // Remove this to see the natural drawing shape
             ..color =
+                // Addition of Opacity gives you the fading effect from dark to light
                 Colors.blue.withRed(i.toInt() * 20 % 11).withOpacity(i / 850));
-      final int tailFibers = (i * 1).toInt();
+
+      // Tail particles effect
+
+      // Change this to add more fibers
+      final int tailFibers = (i * 1.5).toInt();
+
       for (double d = 0; d < area.width; d += tailFibers) {
         for (double e = 0; e < area.height; e += tailFibers) {
-          final n = simplex.noise2D(d, e);
+          final n = random.noise2D(d, e);
           final tail = exp(i / 50) - 5;
           final tailWidth = .2 + (i * .11 * n);
           canvas.drawCircle(
@@ -37,10 +47,13 @@ class Colored extends CustomPainter {
               tailWidth,
               Paint()
                 ..color = Colors.red.withOpacity(.4)
-                ..isAntiAlias = true
+                ..isAntiAlias = true // Change this to lower render time
+                // Particles accelerate as they fall so we change the blur size for movement effect
                 ..imageFilter = ImageFilter.blur(sigmaX: tail, sigmaY: 0)
-                ..filterQuality = FilterQuality.high
-                ..blendMode = BlendMode.screen);
+                ..filterQuality =
+                    FilterQuality.high // Change this to lower render time
+                ..blendMode = BlendMode
+                    .screen); // Remove this to see the natural drawing shape
         }
       }
       canvas.restore();
